5ded63538b27e3f1d5ec74e22c7a451c210c87b9
hamidjavdani <Hamid.Javdany@gmail.com>
2024-02-23 22:09:20 +0330

    insert latestArticle

--- DevCart/Models/Article.cs
+++ DevCart/Models/Article.cs
@@ -5,12 +5,13 @@
         public long Id { get; set; }
         public string Titel { get; set; }
         public string Description { get; set; }
-
-        public Article(long id, string titel, string description)
+        public string Image { get; set; }
+        public Article(long id, string titel, string description,string image)
         {
             Id = id;
             Titel = titel;
             Description = description;
+            Image = image;
         }
     }
 
---
+++ DevCart/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿
+using DevCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCart.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+     new Article(1,"آمورش زبان دات نت","بهترین آموزش زبان","blog-post-thumb-card-2.jpg"),
+     new Article(2,"آمورش زبان انگلیسی","بهترین آموزش زبان","blog-post-thumb-card-3.jpg"),
+     new Article(3,"آمورش گیت","بهترین آموزش زبان","blog-post-thumb-card-4.jpg"),
+            };
+
+            return View("_LetestArticles", articles);
+        }
+    }
+}
